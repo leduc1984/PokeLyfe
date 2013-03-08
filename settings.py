175c5f4a4fe820cde32278d7b463b84c3effee88
Matthew Havard <mh6445a@american.edu>
2013-03-08 16:35:27 -0500

    yup

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 # Django settings for Pokemon project.
-
+import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -45,7 +45,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/templates/'
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'templates')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -106,7 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/templates/'
+    os.path.join(SITE_ROOT, 'templates')
 )
 
 INSTALLED_APPS = (
