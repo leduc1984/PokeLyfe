595154e4efe6eee5b14b9115513825ccf93db109
Matthew Havard <mh6445a@american.edu>
2013-03-10 23:08:17 -0400

    try again

--- settings.py
+++ settings.py
@@ -54,7 +54,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = root('templates')
+MEDIA_ROOT = "../templates/"
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -115,7 +115,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    root('templates')
+    "../templates/"
 )
 
 INSTALLED_APPS = (
