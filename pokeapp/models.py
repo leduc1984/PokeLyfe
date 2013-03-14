7d7d1b23e794196b0655c1a65b7cee02ad6ea704
Matthew Havard <mh6445a@american.edu>
2013-03-14 18:08:02 -0400

    make one-to-one relationship of character to user

--- pokeapp/models.py
+++ pokeapp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Character(models.Model):
+    user = models.OneToOneField(User)
     x = models.IntegerField()
     y = models.IntegerField()
     last_online = models.FloatField()
